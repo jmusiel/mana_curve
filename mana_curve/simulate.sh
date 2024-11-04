1d22ecf7de59f46f729a5b43c420c575af47e65c
jmusiel <73840022+jmusiel@users.noreply.github.com>
2024-11-04 14:26:12 -0500

    remove unnecessary scripts

--- mana_curve/simulate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python simulator.py \
---deck_file /Users/jmusiel/vscode_workspace/personal_projects/mana_curve/mana_curve/kess/top_decklists/deck_1_score_65.json \
---simulations 1000 \
---turns 8 \
---mana_threshold 7 \
---verbose \
--- mana_curve/sweep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python sweep_over_template.py \
---template elenda/elenda_template.json \
---deck_name elenda \
---samples 1000 \
---simulations 500 \
---num_turns 8 \
---force_commander \
