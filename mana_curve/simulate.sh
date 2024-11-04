13ce0d096c08402590aedd1610238880afc2a0b1
jmusiel <73840022+jmusiel@users.noreply.github.com>
2024-11-04 14:22:57 -0500

    update simulate.sh

--- mana_curve/simulate.sh
+++ mana_curve/simulate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python simulator.py \
---deck_file /Users/jmusiel/vscode_workspace/personal_projects/mana_curve/mana_curve/temp_deck_82183_1730419763945.json \
+--deck_file /Users/jmusiel/vscode_workspace/personal_projects/mana_curve/mana_curve/kess/top_decklists/deck_1_score_65.json \
 --simulations 1000 \
---turns 14 \
+--turns 8 \
 --mana_threshold 7 \
---verbose true \
+--verbose \
