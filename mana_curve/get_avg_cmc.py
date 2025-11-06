ec57f36c9434e0464b7bfa04c424954d2d7d1739
jmusiel <73840022+jmusiel@users.noreply.github.com>
2025-11-06 14:29:41 +0100

    more details in avg cmc

--- mana_curve/get_avg_cmc.py
+++ mana_curve/get_avg_cmc.py
@@ -21,6 +21,9 @@ def get_parser():
             "https://archidekt.com/decks/12122030/cantripping_through_time",
             "https://archidekt.com/decks/3390122/will_the_real_mr_markov_please_stand_up",
             "https://archidekt.com/decks/15749155/cunning_conquerors_celerity",
+            "https://archidekt.com/decks/1847823/good_ol_superfriends",
+            "https://archidekt.com/decks/482754/bantchantress",
+            "https://archidekt.com/decks/1856247/lords_landed_libations",
         ],
     )
     return parser
@@ -32,6 +35,16 @@ def card_is_land(card):
         return True
     return False
 
+def print_terminal_histogram(values):
+    max_value = max(values)
+    min_value = min(values)
+    num_bins = max_value - min_value + 1
+    bins = [0] * num_bins
+    for value in values:
+        bins[value - min_value] += 1
+    for i in range(num_bins):
+        print(f"{i + min_value}: {bins[i]}")
+
 def main(config):
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(config)
@@ -40,27 +53,33 @@ def main(config):
     avgcmcs = []
     deck_names = []
     spell_counts = []
+    land_counts = []
     for deck_url in config['deck_urls']:
         deck_name = deck_url.split('/')[-1]
         decklist = get_decklist({'deck_url': deck_url, 'verbose': False})
         decklists.append(decklist)
 
-        avgcmc = 0
+        cmcs = []
         nonland_count = 0
+        land_count = 0
         for card in decklist:
             if not card_is_land(card):
-                avgcmc += card['cmc']
+                cmcs.append(card['cmc'])
                 nonland_count += 1
+            else:
+                land_count += 1
             if not card['cmc'] == card['oracle_cmc']:
                 print(f"{card['name']} cmc: {card['cmc']}")
-        avgcmc /= nonland_count
+        avgcmc = np.mean(cmcs)
         avgcmcs.append(avgcmc)
         deck_names.append(deck_name)
         spell_counts.append(nonland_count)
-        print(f"{deck_name}: avg_cmc: {avgcmcs[-1]} ({nonland_count} spells)")
+        land_counts.append(land_count)
+        print_terminal_histogram(cmcs)
+        print(f"{deck_name}: avg_cmc: {avgcmc} ({nonland_count} spells, {land_count} lands)")
         print("\n")
 
-    df = pd.DataFrame({'deck_name': deck_names, 'spell_counts': spell_counts, 'avg_cmc': avgcmcs})
+    df = pd.DataFrame({'deck_name': deck_names, 'avg_cmc': avgcmcs, 'spell_counts': spell_counts, 'land_counts': land_counts})
     # sort by by avg_cmc
     df = df.sort_values('avg_cmc')
     print(df.to_string())
